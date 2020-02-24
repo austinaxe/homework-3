6b07051a62e5e73d5e29b07e2f65e1b2d8b1eed9
Austin Axe <austinaxelsen@gmail.com>
2020-02-24 17:59:50 -0500

    changed conflicting variable and function - rearranged function underneath variables

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-//function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
-//}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 var char = "abcdefghijklmnopqrstuvwxyz";
 var charUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "1234567890";
@@ -21,25 +6,31 @@ var sym = "!@#$%^&*_-=";
 var charNum = document.getElementById("charNum");
 var numBox = document.getElementById("num");
 var symBox = document.getElementById("sym");
-var submit = document.getElementById("submit");
-var password = document.getElementById("password");
+var upper = document.getElementById("charUp")
+var pwd = document.getElementById("password");
+var generate = document.getElementById("generate");
+
+console.log("TEST!")
+
+function password(l,characters){
+  var pwd = " ";
+
+  for(var i = 0; i<l; i++){
+    console.log("loop")
+    pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return pwd;
+}
 
 generate.addEventListener("click",function(e){
 
   var characters = char;
   (numBox.checked) ? characters += num : "";
   (symBox.checked) ? characters += sym : "";
-  (charUp.checked) ? characters += charUp : "";
-  e.value = password(charNum.value, characters);
+  (upper.checked) ? characters += charUp : "";
+  console.log(charNum.value)
+  pwd.value = password(charNum.value, characters);
+  console.log("called pwd funtion")
+  console.log(password("charNum.value, characters"));
 
 });
-function password(l,characters){
-  var pwd = " ";
-
-  for(var i = 0; i>l; i++){
-    pwd += characters.charAt(Math.floor(Math.random() * characters.length));
-
-  }
-  return pwd;
-}
-      
